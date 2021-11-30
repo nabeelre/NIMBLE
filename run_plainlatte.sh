88b8423d84efcec2751297c2a832612ec6fc058f
Nabeel Rehemtulla <33795390+nabeelre@users.noreply.github.com>
2021-11-29 20:54:27 -0700

    Delete run_plainlatte.sh

--- run_plainlatte.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 jeans_bspline.py data/m12f_prejeans.csv data/m12f_true.csv
-python3 jeans_bspline.py data/m12i_prejeans.csv data/m12i_true.csv
-python3 jeans_bspline.py data/m12m_prejeans.csv data/m12m_true.csv
