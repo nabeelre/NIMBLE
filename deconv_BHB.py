5ca535a6ac3ba0f8c9a4dd575fcd211bb0a7e98b
Nabeel Rehemtulla <nabeelre@gmail.com>
2024-08-16 11:21:13 -0400

    some linting and work on BHB conversion

--- deconv_BHB.py
+++ deconv_BHB.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 from multiprocess import Pool
 from datetime import datetime
+import scipy.optimize
+import scipy.special
 import pandas as pd
 import numpy as np
 import corner
@@ -10,11 +12,10 @@ import time
 import sys
 import os
 
-import scipy.special, scipy.optimize
-
 import latte_helpers as latte
 import auridesi_helpers as auridesi
 import iron_helpers as iron
+from deconv import getSurveyFootprintBoundary, write_csv, parse_knots
 
 np.set_printoptions(linewidth=200, precision=6, suppress=True)
 np.random.seed(42)
@@ -22,33 +23,30 @@ np.random.seed(42)
 SUBSAMPLE = False
 VERBOSE = True
 
-Gmax  = 20.7   # max apparent G magnitude for a RRL star to enter the selection
-Gmin  = 16.0   # min apparent G magnitude (exclude nearby stars)
-Grrl  = 0.58   # abs.magnitude of RRL (mean value; the actual value for each star is scattered around it) From Iorio&Belokurov2018
-DMerr = 0.24   # scatter in abs.magnitude
+Grrl = 0.58   # TODO
+DMerr = 0.24   # TODO? scatter in abs.magnitude
 # assume that the error in distance modulus is purely due to intrinsic scatter in abs.mag
 # (neglect photometric measurement errors, which are likely much smaller even at G=20.7)
-bmin  = 30.0   # min galactic latitude for the selection box (in degrees)
-decmin=-35.0   # min declination for the selection box (degrees)
-d2r   = np.pi/180  # conversion from degrees to radians
+bmin = 30.0   # min galactic latitude for the selection box (in degrees)
+decmin = -35.0   # min declination for the selection box (degrees)
+d2r = np.pi/180  # conversion from degrees to radians
 
-min_knot  = 5   # kpc
-max_knot  = 80  # kpc
+min_knot = 5   # kpc
+max_knot = 80  # kpc
 num_knots = 5
 
-min_r     = 1   # kpc
-max_r     = 70  # kpc
-use_external_density = False
+min_r = 1   # kpc
+max_r = 70  # kpc
 
 
-def medina24rrl_rho(radii):
+def medina24rrl_rho(radii):  # TODO, replace with BHB profile
     # https://ui.adsabs.harvard.edu/abs/2024MNRAS.531.4762M/abstract
     R_break = 18.0
     slope_inner = -2.05
     slope_outer = -4.47
     A1 = 0.67
     A2 = 1.52
-    
+
     if isinstance(radii, float) or isinstance(radii, int):
         radii = [radii]
 
@@ -58,16 +56,16 @@ def medina24rrl_rho(radii):
             log10dens[i] = A1 + slope_inner*np.log10(r/8)
         else:
             log10dens[i] = A2 + slope_outer*np.log10(r/8)
-            
+
     return 10**log10dens
 
 
-def medina24rrl_dlnrho(log_radii):
+def medina24rrl_dlnrho(log_radii):  # TODO, replace with BHB profile
     # https://ui.adsabs.harvard.edu/abs/2024MNRAS.531.4762M/abstract
     R_break = 18.0
     slope_inner = -2.05
     slope_outer = -4.47
-    
+
     if isinstance(log_radii, float) or isinstance(log_radii, int):
         log_radii = [log_radii]
 
@@ -77,115 +75,21 @@ def medina24rrl_dlnrho(log_radii):
             dlnrho_dlnr[i] = slope_inner
         else:
             dlnrho_dlnr[i] = slope_outer
-            
+
     return dlnrho_dlnr
-
-
-def write_csv(data, filename, column_titles):
-    np.savetxt(
-        fname=figs_path+filename, X=data, delimiter=',', header=column_titles
-    )
-
-
-def getSurveyFootprintBoundary(decmin):
-    # determine the range of l as a function of b, resulting from a cut in declination dec>=decmin
-    # (note: the approach is tuned specifically to a constant boundary in declination,
-    # but should work for any value of decmin from -90 to +90)
-    if decmin == -90:  # no restrictions
-        return -np.pi/2, np.pi/2, lambda b: b*0, np.pi
-
-    # 1. obtain the l,b coords of the two poles (south and north) in ICRS
-    lp, bp = agama.transformCelestialCoords(agama.fromICRStoGalactic, [0, 0], [-np.pi/2, np.pi/2])
-    # if decmin < decp[0], there is only one loop around the south ICRS pole,
-    # likewise if decmin > decp[1], there is only one loop around the north ICRS pole,
-    # otherwise there is a boundary spanning the entire range of l
-
-    # 2. find latitude at which boundary crosses the meridional lines in l,b containing the poles
-    def rootfinder(b, l0):
-        return agama.transformCelestialCoords(agama.fromGalactictoICRS, l0, b)[1] - decmin*d2r
-    if decmin*d2r < bp[0]:
-        b1 = scipy.optimize.brentq(rootfinder, bp[0], -np.pi/2, args=(lp[0],))
-    else:
-        b1 = scipy.optimize.brentq(rootfinder, bp[1], -np.pi/2, args=(lp[1],))
-    if decmin*d2r > bp[1]:
-        b2 = scipy.optimize.brentq(rootfinder, bp[1], +np.pi/2, args=(lp[1],))
-    else:
-        b2 = scipy.optimize.brentq(rootfinder, bp[0], +np.pi/2, args=(lp[0],))
-
-    # 3. construct a boundary - minimum l for each b between b1 and b2
-    npoints = 201
-    bb = np.linspace(0, 1, npoints)
-    bb = bb*bb*(3-2*bb) * (b2-b1) + b1
-    ll = np.zeros(npoints)
-    ll[0] = lp[0] if decmin*d2r < bp[0] else lp[1]
-    ll[-1] = lp[1] if decmin*d2r > bp[1] else lp[0]
-    for i in range(1, npoints-1):
-        ll[i] = scipy.optimize.brentq(
-            lambda lat: agama.transformCelestialCoords(agama.fromGalactictoICRS,
-                                                       lat, bb[i])[1] - decmin*d2r,
-            lp[0], lp[1])
-    curve = agama.CubicSpline(bb, ll)
-
-    # 4. return a tuple of four elements:
-    #   lower and upper limit on b,
-    #   a function evaluating lmin for the given b,
-    #   and lsym such that lmax(b) = 2*lsym - lmin(b).
-    # note all coords here in radians, and the range of l is from lsym-pi to lsym+pi (or smaller),
-    # not from 0 to 2pi or -pi to pi; thus selection boundary doesn't enclose the actual coordinates
-    # of points unless these are shifted to the same angular range, but this doesn't matter for
-    # computing the normalization factor (integral of density over the selection region)
-    if decmin*d2r < bp[0]:
-        # excluded region inside a closed loop around South pole; b anywhere between -pi/2 and pi/2
-        return -np.pi/2, np.pi/2, lambda b: curve(b, ext=lp[0]), lp[1]
-    elif decmin*d2r <= bp[1]:
-        # excluded a region below a curve spanning the entire range of l; b must be >= b1
-        return b1, np.pi/2, lambda b: curve(b, ext=lp[0]), lp[1]
-    else:  # excluded a region outside a closed loop around North pole; b must be between b1 and b2
-        return b1, b2, lambda b: curve(b, ext=lp[1]), lp[1]
-
-
-def parse_knots(argv):
-    min_k = int(argv[0])
-    max_k = int(argv[1])
-    num_k = int(argv[2])
-
-    assert(max_k > min_k)
-    assert(num_k > 0)
-
-    print(f"Knots overridden (min,max,num)=({min_k},{max_k},{num_k})")
-    return (min_k, max_k, num_k)
 
 
 def parse_args(argv):
     kind = argv[1]
-    global min_knot, max_knot, num_knots, min_r, max_r, Gmax, bmin
+    global min_knot, max_knot, num_knots, min_r, max_r, Gmax, Gmin, bmin
     knot_override = None
 
-    if kind == "latte":
-        lattesim = argv[2].lower()
-        lsr = argv[3].upper()
-        gaia_release = argv[4].upper()
-
-        assert(gaia_release in ['DR3', 'DR4', 'DR5'])
-        assert(lattesim in ['m12f', 'm12i', 'm12m'])
-        assert(lsr in ['LSR0', 'LSR1', 'LSR2'])
-
-        print(f"\033[1;33m**** RUNNING LATTE {lattesim} {lsr} {gaia_release} ****\033[0m")
-
-        figs_path = f"results/latte/deconv_{lattesim}_{lsr}_{gaia_release}/"
-        true_path = f"data/{lattesim}/{lattesim}_true.csv"
-
-        load_params = (lattesim, lsr, gaia_release)
-        load_fnc = latte.load
-
-        if len(argv) == 8:
-            knot_override = parse_knots(argv[5:])
-    elif kind == "auridesi":
+    if kind == "auridesi":
         halonum = argv[2].lower()
         lsrdeg = argv[3].upper()
 
-        assert(halonum in ["06", "16", "21", "23", "24", "27"])
-        assert(lsrdeg in ["030", "120", "210", "300"])
+        assert (halonum in ["06", "16", "21", "23", "24", "27"])
+        assert (lsrdeg in ["030", "120", "210", "300"])
 
         print(f"\033[1;33m**** RUNNING AURIDESI {halonum} {lsrdeg} ****\033[0m")
 
@@ -197,7 +101,7 @@ def parse_args(argv):
 
         Gmax = 19.0
         auridesi.Gmax = Gmax
-        
+
         bmin = 30
         auridesi.bmin = bmin
 
@@ -210,7 +114,7 @@ def parse_args(argv):
         load_fnc = iron.load
         load_params = ()
 
-        print(f"\033[1;33m**** RUNNING IRON ****\033[0m")
+        print("\033[1;33m**** RUNNING IRON ****\033[0m")
 
         figs_path = "results/iron/"
         true_path = None
@@ -225,10 +129,10 @@ def parse_args(argv):
             knot_override = parse_knots(argv[2:])
 
     if knot_override is not None:
-        min_knot  = knot_override[0]
-        max_knot  = knot_override[1]
+        min_knot = knot_override[0]
+        max_knot = knot_override[1]
         num_knots = knot_override[2]
-        
+
     timestamp = datetime.now().strftime('%m%d%y_%H%M')
     figs_path += "_".join(map(str, (min_knot, max_knot, num_knots)))+"/"
     figs_path += timestamp + "/"
@@ -240,14 +144,14 @@ def parse_args(argv):
 if __name__ == "__main__":
     kind, load_fnc, load_params, figs_path, true_path = parse_args(sys.argv)
 
+    # TODO: pull in distance, some apparent mag
     l, b, true_dens_radii, Gapp, pml, pmb, vlos, PMerr, vloserr, true_sigmar, true_sigmat, \
         lsr_info = load_fnc(*load_params, SUBSAMPLE, VERBOSE)
 
-    if VERBOSE: 
+    if VERBOSE:
         print('Number of particles in survey volume:', len(l))
         print('Gmax:', Gmax)
         print('Gmin:', Gmin)
-        print('DMerr:', DMerr)
         print('bmin:', bmin)
         print('decmin:', decmin)
         print('min_r:', min_r)
@@ -258,13 +162,15 @@ if __name__ == "__main__":
 
     if not os.path.exists(figs_path):
         os.makedirs(figs_path)
-        if VERBOSE: print("created output directory for figures at " + figs_path)
+        if VERBOSE:
+            print("created output directory for figures at " + figs_path)
     print()
 
     blow, bupp, lmin, lsym = getSurveyFootprintBoundary(decmin)
 
     # diagnostic plot showing the stars in l,b and the selection region boundary
     if VERBOSE:
+        # TODO: replace Gapp with new apparent mag from Amanda's catalog
         plt.scatter(l, b, s=2, c=Gapp, cmap='hell', vmin=Gmin, vmax=Gmax+1, edgecolors='none', rasterized=True)
         plt.colorbar(label='Gapp')
         if blow <= -bmin*d2r:  # selection region in the southern Galactic hemisphere
@@ -284,8 +190,6 @@ if __name__ == "__main__":
         plt.savefig(figs_path+"sel_bounds.pdf", dpi=200, bbox_inches='tight')
         plt.cla()
 
-    # this used to be a function, but the MCMC parallelization doesn't work unless the likelihood fnc
-    # is in the global scope (possibly fixed in the latest EMCEE version - haven't checked)
     def modelDensity(params, truedens=False):
         # params is the array of logarithms of density at radial knots, which must monotonically decrease
         # note that since the result is invariant w.r.t. the overall amplitude of the density
@@ -312,9 +216,8 @@ if __name__ == "__main__":
 
     def likelihood(params):
         # function to be maximized in the MCMC and deterministic optimization
-        params_dens   = params[0 : len(knots_logr)-1]
-        params_sigmar = params[len(knots_logr)-1 : 2*len(knots_logr)-1]
-        params_sigmat = params[2*len(knots_logr)-1 :]
+        params_sigmar = params[0 : len(knots_logr)-1]
+        params_sigmat = params[len(knots_logr)-1 : 2*len(knots_logr)-1]
         try:
             # construct intrinsic velocity dispersion profiles of the model
             sigmar2   = np.exp(2*modelSigma(params_sigmar)(logr_samp))  # squared radial velocity dispersion
@@ -339,7 +242,8 @@ if __name__ == "__main__":
             # then sum up marginalized log-likelihoods of all stars.
             # at this stage may also add a prior if necessary
             loglikelihood = np.sum(np.log(
-                np.mean((like_pm * like_vlos).reshape(npoints, nsamples), axis=1)))
+                np.mean((like_pm * like_vlos).reshape(npoints, nsamples), axis=1)
+            ))
             #print("%s => %.2f" % (params, loglikelihood))
             if not np.isfinite(loglikelihood): loglikelihood = -np.inf
             return loglikelihood
@@ -375,16 +279,17 @@ if __name__ == "__main__":
                 axs[0].set_ylim(min(truedens)*0.2, max(truedens)*2)
             if use_external_density:
                 axs[0].plot(r, medina24rrl_rho(r))
-            # retrieve density profiles of each model in the chain, and compute median and 16/84 percentiles
-            results = np.zeros((len(chain), len(r)))
-            for i in range(len(chain)):
-                results[i] = np.exp(modelDensity(chain[i, 0:len(knots_logr)-1])(lr))
-            dens_low, dens_med, dens_upp = np.percentile(results, [16,50,84], axis=0)
-            # plot the model profiles with 1sigma confidence intervals
-            axs[0].fill_between(r, dens_low, dens_upp, alpha=0.3, lw=0, color='r')
-            axs[0].plot(r, dens_med, color='r', label='MCMC Fit')
+            # TODO: add plotting of assumed mass profile here
+            # # retrieve density profiles of each model in the chain, and compute median and 16/84 percentiles
+            # results = np.zeros((len(chain), len(r)))
+            # for i in range(len(chain)):
+            #     results[i] = np.exp(modelDensity(chain[i, 0:len(knots_logr)-1])(lr))
+            # dens_low, dens_med, dens_upp = np.percentile(results, [16,50,84], axis=0)
+            # # plot the model profiles with 1sigma confidence intervals
+            # axs[0].fill_between(r, dens_low, dens_upp, alpha=0.3, lw=0, color='r')
+            # axs[0].plot(r, dens_med, color='r', label='MCMC Fit')
             count_obs = np.histogram(logr_obs, bins=lrhist)[0]
-            rho_obs = count_obs / (4 * np.pi * (lrhist[1:]-lrhist[:-1]) * (rhist[1:]*rhist[:-1])**1.5 * len(Gapp))
+            rho_obs = count_obs / (4 * np.pi * (lrhist[1:]-lrhist[:-1]) * (rhist[1:]*rhist[:-1])**1.5 * len(logr_obs))
             axs[0].plot(np.repeat(rhist,2)[1:-1], np.repeat(rho_obs, 2), 'r--', label='with SFs and Errors')
 
             axs[0].set_title(figs_path)
@@ -479,11 +384,11 @@ if __name__ == "__main__":
         if plotname == 'converged':
             # True and MCMC velocity dispersion and density profiles
             if true_path is not None:
-                profiles = np.stack([r, dens_low, dens_med, dens_upp, truedens, low_r, med_r, upp_r, truesigr, low_t, med_t, upp_t, truesigt], axis=1)
-                write_csv(profiles, "veldisp_dens_profiles.csv", "r, dens_low, dens_med, dens_upp, truedens, low_r, med_r, upp_r, truesigr, low_t, med_t, upp_t, truesigt")
+                profiles = np.stack([r, truedens, low_r, med_r, upp_r, truesigr, low_t, med_t, upp_t, truesigt], axis=1)
+                write_csv(profiles, "veldisp_dens_profiles.csv", "r, truedens, low_r, med_r, upp_r, truesigr, low_t, med_t, upp_t, truesigt")
             else:
-                profiles = np.stack([r, dens_low, dens_med, dens_upp, low_r, med_r, upp_r, low_t, med_t, upp_t], axis=1)
-                write_csv(profiles, "veldisp_dens_profiles.csv", "r, dens_low, dens_med, dens_upp, low_r, med_r, upp_r, low_t, med_t, upp_t")
+                profiles = np.stack([r, low_r, med_r, upp_r, low_t, med_t, upp_t], axis=1)
+                write_csv(profiles, "veldisp_dens_profiles.csv", "r, low_r, med_r, upp_r, low_t, med_t, upp_t")
 
             # histograms of velocity dispersion and density after obs effects
             histograms = np.stack([np.repeat(rhist,2)[1:-1], np.repeat(rho_obs, 2), np.repeat(sigmar_obs, 2), np.repeat(sigmat_obs, 2)], axis=1)
@@ -493,7 +398,7 @@ if __name__ == "__main__":
 
     # convert l,b,dist.mod. of all stars into logarithm of Galactocentric radius (observed, not true)
     # unit conversion: degrees to radians for l,b,  mas/yr to km/s/kpc for PM
-    dist_obs = 10**(0.2*(Gapp-Grrl)-2)
+    dist_obs = 10**(0.2*(Gapp-Grrl)-2)  # TODO: pull distance from amanda's sample
     x, y, z, vx, vy, vz = agama.getGalactocentricFromGalactic(
         l*d2r, b*d2r, dist_obs, pml*4.74, pmb*4.74, vlos, *lsr_info)
     logr_obs = 0.5 * np.log(x**2 + y**2 + z**2)
@@ -503,7 +408,7 @@ if __name__ == "__main__":
     # create random samples from distance modulus unc for each star and convert to Galactocentric r
     nsamples = 20  # number of random samples per star
     Gsamp = (np.random.normal(size=(npoints, nsamples)) * DMerr + Gapp[:, None]).reshape(-1)
-    dist_samp = 10**(0.2*(Gsamp-Grrl)-2)
+    dist_samp = 10**(0.2*(Gsamp-Grrl)-2)  # TODO: instead, impose error directly in distance - based on spread for GC / dwarf?
     x, y, z = agama.getGalactocentricFromGalactic(
         np.repeat(l * d2r, nsamples), np.repeat(b * d2r, nsamples),
         dist_samp, galcen_distance=lsr_info[0], galcen_v_sun=lsr_info[1], z_sun=lsr_info[2]
@@ -557,14 +462,11 @@ if __name__ == "__main__":
     knots_logr = np.linspace(np.log(min_knot), np.log(max_knot), num_knots)
 
     # initial values of parameters
-    # log of (un-normalized) density values at radial knots
-    params_dens = -np.linspace(1, 3, len(knots_logr)-1)**2
     # log of radial velocity dispersion values at the radial knots
     params_sigmar = np.zeros(len(knots_logr)) + 5.0
     params_sigmat = np.zeros(len(knots_logr)) + 5.0   # same for tangential dispersion
-    params = np.hstack((params_dens, params_sigmar, params_sigmat))
-    paramnames = ['logrho(r=%4.1f)' % r for r in np.exp(knots_logr[1:])] + \
-        ['sigmar(r=%4.1f)' % r for r in np.exp(knots_logr)] + \
+    params = np.hstack((params_sigmar, params_sigmat))
+    paramnames = ['sigmar(r=%4.1f)' % r for r in np.exp(knots_logr)] + \
         ['sigmat(r=%4.1f)' % r for r in np.exp(knots_logr)]
     prevmaxloglike = -np.inf
     prevavgloglike = -np.inf
@@ -696,15 +598,15 @@ if __name__ == "__main__":
     # Thin the chain
     chain_smpl = chain[::20]
 
-    dlnrho, dlnsigr, sigr, sigt = np.zeros((4, len(chain_smpl), len(r)))
+    dlnsigr, sigr, sigt = np.zeros((3, len(chain_smpl), len(r)))
     for i in range(len(chain_smpl)):
         if not use_external_density:
             dlnrho[i] = modelDensity(chain_smpl[i, 0:len(knots_logr)-1])(lr, der=1)
         else:
             dlnrho[i] = medina24rrl_dlnrho(lr)
-        dlnsigr[i] = modelSigma(chain_smpl[i, len(knots_logr)-1 : 2*len(knots_logr)-1])(lr, der=1)
-        sigr   [i] = np.exp(modelSigma(chain_smpl[i, len(knots_logr)-1 : 2*len(knots_logr)-1])(lr))
-        sigt   [i] = np.exp(modelSigma(chain_smpl[i, 2*len(knots_logr)-1 :])(lr))
+        dlnsigr[i] = modelSigma(chain_smpl[i, 0:len(knots_logr)-1])(lr, der=1)
+        sigr   [i] = np.exp(modelSigma(chain_smpl[i, 0:len(knots_logr)-1])(lr))
+        sigt   [i] = np.exp(modelSigma(chain_smpl[i, len(knots_logr)-1 : 2*len(knots_logr)-1])(lr))
 
     Mencs, betas = np.zeros((2, len(chain_smpl), len(r)))
     for i in range(len(chain_smpl)):
